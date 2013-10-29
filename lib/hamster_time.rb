2514316cd424bb51e4570db4b59e81fa4f493a42
Gatis Tomsons <gatis.tomsons@gmail.com>
2013-10-30 00:44:54 +0200

    marks added facts; fixed bug with day overlapping;

--- lib/hamster_time.rb
+++ lib/hamster_time.rb
@@ -1,5 +1,3 @@
-require "active_support/core_ext/date/calculations"
-require "active_support/core_ext/time/calculations"
 require "ostruct"
 
 require "hamster_time/version"
@@ -27,8 +25,8 @@ module HamsterTime
 
     def collect_activities
       @activities = Hamster::Fact.includes(:activity).joins(:activity).where(%^
-        start_time > ? AND end_time < ? AND synced IS NOT 1
-        ^, report_day.beginning_of_day, report_day.end_of_day
+        DATE(start_time) = ? AND synced IS NOT 1
+        ^, report_day
       )
     end
 
@@ -36,6 +34,7 @@ module HamsterTime
       site.sign_in
       activities.each do |_, activity|
         if site.add_activity activity
+          mark_as_synced(activity)
           puts "Added activity: #{activity.name}"
         else
           puts "Failed to add activity: #{activity.name}"
@@ -58,10 +57,12 @@ module HamsterTime
             hours: round_time(item.end_time - item.start_time),
             start_time: item.start_time,
             category: item.activity.category.name,
-            description: "#{item.tag_names}, #{item.activity.name[/#.+/] || item.activity.name}"
+            description: "#{item.tag_names}, #{item.activity.name[/#.+/] || item.activity.name}",
+            fact_ids: [item.id]
           })
         else
           result[item.activity_id].hours += round_time(item.end_time - item.start_time)
+          result[item.activity_id].fact_ids << item.id
         end
         result
       end
@@ -69,6 +70,10 @@ module HamsterTime
 
     def round_time diff
       ((diff / 60 / 60).round(2) / 0.25).round * 0.25
+    end
+
+    def mark_as_synced activity
+      Hamster::Fact.where(id: activity.fact_ids).update_all("synced = 1")
     end
   end
 end
